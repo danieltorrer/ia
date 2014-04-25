5021202ad4a1e365f9adf7127984696dd7b22fd6
Daniel <monkyponky@hotmail.com>
2014-04-25 13:07:27 -0500

    asd

--- js/map.js
+++ js/map.js
@@ -104,10 +104,11 @@ options = {
 },
 tipo = "4deefb944765f83613cdba6e",
 precio = "1",
-radius = "5000",
+radius = "8000",
 div = document.getElementById("map"),
 map = null,
-query = ""
+query = "",
+markers = [];
 
 //google.maps.event.addDomListener(window, "load", initialize);
 
@@ -171,10 +172,13 @@ $(document).ready(function(){
 
 
 function initialize() {
-	map.mapTypes.set("Styled", styledMapType), new google.maps.InfoWindow, $.getJSON(query, buildList)
+	map.mapTypes.set("Styled", styledMapType), 
+	new google.maps.InfoWindow, 
+	$.getJSON(query, buildList)
+	console.log(query);
 }
 
-function createMarker(a, b, c, d, e, id) {
+function createMarker(a, b, c, d, e, id, cont) {
 	var f = new google.maps.Marker({
 		map: map,
 		position: {
@@ -187,11 +191,14 @@ function createMarker(a, b, c, d, e, id) {
 		infowindow.setContent(c + "<br><span class='subtitle'>" + d + "</h6>"), infowindow.open(map, this)
 	})
 
-	var lugar = "<div class='lugar'>"
+	markers.push(f);
+
+	var lugar = "<div class='lugar' data-lat='" + a + "' data-lng='"+ b +"' data-id='"+ cont +"'>"
 	lugar += "<img src='" + e + "bg_64.png'>" 
 	lugar += "<span>" + c + "</span>" 
-	lugar += "<i class='ocultar boton-add right fi-plus' href='#' data-id='"+ id + "'></i>" 
+	lugar += "<i class='ocultar boton-add right fi-plus' data-id='"+ id + "' href='#' id='"+ cont +"''></i>" 
 	lugar += "</div>"
+
 	$(".lugares").append(lugar)
 
 	$(".lugar").mouseover(function(){
@@ -202,14 +209,30 @@ function createMarker(a, b, c, d, e, id) {
 		$(this).children(".boton-add").addClass("ocultar")
 	})
 
+	$(".boton-add").unbind().click(addLugar)
+	$(".lugar").click(centerMap)
 
+
+}
+
+function addLugar(){
+	//console.log(lel);
+	console.log(event.target.id)
+	$( this ).parent().clone().appendTo( ".seleccion" );
+	
+}
+
+function centerMap(){
+	var point = new google.maps.LatLng($(this).attr("data-lat"),$(this).attr("data-lng"))
+	map.setCenter(point)
+	google.maps.event.trigger(markers[$(this).attr("data-id")], 'click');
 }
 
 function buildList(a) {
 	infowindow = new google.maps.InfoWindow
 
 	$.each(a.response.groups[0].items, function (a, b) {
-		createMarker(b.venue.location.lat, b.venue.location.lng, b.venue.name, b.venue.categories[0].shortName, b.venue.categories[0].icon.prefix, b.venue.id)
+		createMarker(b.venue.location.lat, b.venue.location.lng, b.venue.name, b.venue.categories[0].shortName, b.venue.categories[0].icon.prefix, b.venue.id,a)
 		console.log(b)
 	})
 }
